49030ca61da4f2898df80dda16c15a9d4bde33cf
Jonathan Schafeitel <jonathan_schafeitel@hotmail.ch>
2022-12-07 18:37:14 +0100

    work on devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_devise_parameters, if: :devise_controller?
+    add_flash_types :info, :error, :warning, :success, :primary, :secondary, :danger, :light, :dark
 
     def configure_devise_parameters
       devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :is_alive, :email, :password, :password_confirmation)}
