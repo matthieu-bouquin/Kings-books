8e2f008ae2262498c2c6700615c135520cf76284
JERRY farwell <114482666+jerryfarwell@users.noreply.github.com>
2022-12-08 13:29:27 +0100

    ruby version in gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_102739) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "plan"
+    t.string "stripe_customer_id"
+    t.string "session_token"
+    t.string "string"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -55,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_102739) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["stripe_customer_id"], name: "index_users_on_stripe_customer_id", unique: true
   end
 
 end
