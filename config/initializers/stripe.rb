030c0e5f0db925926d61892f6016bd50adcd8d73
Jonathan Schafeitel <jonathan_schafeitel@hotmail.ch>
2022-12-09 16:35:47 +0100

    Update stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-    :publishable_key => "pk_test_51M2bQOEdMYtPZIQq6B236XNATf62pxRoOhnncokvomDTfIqcN5SwW77KW6gOaVc5n6BVTcaUPv3NVJBNlWC8XgZg00pGsFTbjG",
-    :secret_key      => "sk_test_51M2bQOEdMYtPZIQqxVytkQRx9Zlh0qxsX9KjUwzz8uRGWWE0LMMR0IiHSgIAmhgwhelwa0NOGEXTV5gdn5Ch8prw00ZzetDaJm"
+    :publishable_key => ENV['PUBLISHABLE_KEY'],
+    :secret_key      => ENV['SECRET_KEY']
   }
   Stripe.api_key = Rails.configuration.stripe[":secret_key"]
